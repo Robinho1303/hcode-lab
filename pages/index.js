e69568d4e78c5a61d784546863060eb426fbd69d
Robson <Robinho.cleber@gmail.com>
2020-04-24 20:40:56 -0300

    Iniciando

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+
+export default function Index () {
+  return(
+
+
+    <div>
+      
+      <Header />
+        <h1>Estou na Index</h1>
+    </div>
+  )
+}
